11065b3df317e430a2c8785c7f9605b10115be72
PWhite-Eng <75939018+PWhite-Eng@users.noreply.github.com>
2026-01-01 18:16:58 +0000

    feat(gateway): add trace queue to GatewayHandler interface
    Updated GatewayHandler to accept a new FreeRTOS queue handle for
    trace/debug messages. This allows invalid RF packets and error
    codes to be passed to the main application for MQTT publishing.

--- lib/GatewayHandler/GatewayHandler.hpp
+++ lib/GatewayHandler/GatewayHandler.hpp
@@ -34,6 +34,7 @@ private:
     QueueHandle_t _txStringQueueHandle;
     QueueHandle_t _cmdStringQueueHandle;
     QueueHandle_t _publishQueueHandle;
+    QueueHandle_t _traceQueueHandle;
     const uint8_t _GWAY_CLASS;
     const uint8_t _MQTT_MSG_MAX_LEN;
 
@@ -43,6 +44,7 @@ private:
     // --- Private Method Declarations ---
     void setLedState(LedState newState);
     void publishRadioPacket(const char* message);
+    void publishTrace(const char* message);
     void handleHostSerial();
     void handleMqttTx();
     void handleMqttCmd();
@@ -60,14 +62,15 @@ public:
      * @param txQueue Handle to the MQTT->Radio TX queue.
      * @param cmdQueue Handle to the MQTT->Command queue.
      * @param pubQueue Handle to the Radio->MQTT publish queue.
+     * @param traceQueue Handle to the trace/logging queue.
      * @param gwayClass The gateway's class ID (e.g., 18).
      * @param maxMsgLen The maximum length for an MQTT message.
      * @param callback A function pointer to call for publishing command results.
      */
     GatewayHandler(EvofwProtocol& proto, EvofwHandler& handler, Stream& host, 
                    QueueHandle_t ledQueue, QueueHandle_t txQueue, QueueHandle_t cmdQueue, 
-                   QueueHandle_t pubQueue, const uint8_t gwayClass, const uint8_t maxMsgLen,
-                   CommandResultCallback callback);
+                   QueueHandle_t pubQueue, QueueHandle_t traceQueue,const uint8_t gwayClass, 
+                   const uint8_t maxMsgLen, CommandResultCallback callback);
     
     /**
      * @brief Initializes the handler.
